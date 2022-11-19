ef2dcb8aac43da8bdb6e91882de526066e915ae7
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2022-11-19 15:31:55 -0300

    Refactor: remove unnecessary comments

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,24 +1,10 @@
-// Checklist para subir no Git hub/Linkedin
-
-/*
-    1. Alterar a regra de negócio do game, deixá-lo mais intuitivo
-    2. Diminuir número de tentativas
-    3. Tornar o projeto responsivo
-    4. Refatorar o que for possível, usando o princípio DRY - function updateElementOnScreen
-    5. Adicionar Modal com instruções sobre o jogo
-    6. Montar README no novo padrão
-    7. Atualizar profile do Git Hub
-    8. Atualizar profile do Linkedin
-*/
-
 'use strict';
 
 let secretNumber = getSecretNumber();
-
 let score = Number(document.querySelector('.score').textContent);
 let highscore = Number(document.querySelector('.highscore').textContent);
 
-const displayMessage = function(message) {
+const displayMessage = function (message) {
     document.querySelector('.message').textContent = message;
 }
 
@@ -26,52 +12,47 @@ function getSecretNumber() {
     return Math.trunc(Math.random() * 20);
 }
 
-document.querySelector('.check').addEventListener('click', function() {
+/* --------------------------
+        Check number
+-------------------------- */
+document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
 
-    // When there is no value
-    if(!guess){
+    if (!guess) {
         displayMessage('📛 No Number!');
-        
-        // When player wins
     } else if (guess === secretNumber) {
-
         document.querySelector('.number').textContent = secretNumber;
-        displayMessage('✔ Correct Number!');  
-
+        displayMessage('✔ Correct Number!');
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
-
-        if(score > highscore){
+        
+        if (score > highscore) {
             highscore = score;
             document.querySelector('.highscore').textContent = highscore;
         }
-
-        // When guess is wrong
-    } else if(guess !== secretNumber){
-        if(score > 1){
+    } else if (guess !== secretNumber) {
+        if (score > 1) {
             displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 To low!');
-            score --;
+            score--;
             document.querySelector('.score').textContent = score;
         } else {
             displayMessage('You lost the game!');
             document.querySelector('.score').textContent = 0;
         }
-    } 
+    }
 });
 
-// Reset the game
-
-document.querySelector('.again').addEventListener('click', function() {
-
+/* --------------------------
+        Reset the game
+-------------------------- */
+document.querySelector('.again').addEventListener('click', function () {
     score = 20;
     secretNumber = getSecretNumber();
-    
+
     document.querySelector('.number').textContent = '?';
     displayMessage('Start guessing...');
     document.querySelector('.score').textContent = score;
     document.querySelector('.guess').value = '';
-
     document.querySelector('body').style.backgroundColor = '#222';
     document.querySelector('.number').style.width = '15rem';
 });
