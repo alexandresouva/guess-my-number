8e533dd8238257ff481dcd884e4bdfacb9a7f456
alexandresouva <pseudoalexandre@gmail.com>
2024-05-14 22:48:42 -0300

    feat: update DOM on correct guess

--- script.js
+++ script.js
@@ -3,33 +3,36 @@
 const secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
 
-const checkBtnEl = document.querySelector('.check');
-const scoreEl = document.querySelector('.score');
+const bodyEl = document.querySelector('body');
+const numberEl = document.querySelector('.number');
+const guessEl = document.querySelector('.guess');
 const messageEl = document.querySelector('.message');
+const scoreEl = document.querySelector('.score');
+const checkBtnEl = document.querySelector('.check');
 
 scoreEl.textContent = score;
-document.querySelector('.number').textContent = secretNumber;
+console.log(secretNumber);
 checkBtnEl.addEventListener('click', checkNumber);
 
 function checkNumber() {
-  const guess = Number(document.querySelector('.guess').value);
+  const guess = Number(guessEl.value);
   if (!guess) {
     messageEl.textContent = '🤡 Choice a number.';
   } else if (guess === secretNumber) {
-    messageEl.textContent = '🎉 Correct number!';
+    updateViewOnGuessSucess();
   } else if (guess > secretNumber) {
     if (score > 1) {
       messageEl.textContent = '📈 Too high!';
       decreaseScore();
     } else {
-      printGameOver();
+      printGameOverMessage();
     }
   } else if (guess < secretNumber) {
     if (score > 1) {
       messageEl.textContent = '📉 Too low!';
       decreaseScore();
     } else {
-      printGameOver();
+      printGameOverMessage();
     }
   }
 }
@@ -39,7 +42,14 @@ function decreaseScore() {
   scoreEl.textContent = score;
 }
 
-function printGameOver() {
+function printGameOverMessage() {
   messageEl.textContent = '🫤 Game over...';
   scoreEl.textContent = 0;
+}
+
+function updateViewOnGuessSucess() {
+  messageEl.textContent = '🎉 Correct number!';
+  numberEl.textContent = secretNumber;
+  bodyEl.style.backgroundColor = '#60b347';
+  numberEl.style.width = '30rem';
 }
