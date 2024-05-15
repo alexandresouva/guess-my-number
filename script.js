1d0890ac0d8c18ef8bd810ef1cddf68cb1c37855
alexandresouva <pseudoalexandre@gmail.com>
2024-05-14 22:56:38 -0300

    feat: reset the game

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let secretNumber = generateSecretNumber();
 let score = 20;
 
 const bodyEl = document.querySelector('body');
@@ -9,17 +9,19 @@ const guessEl = document.querySelector('.guess');
 const messageEl = document.querySelector('.message');
 const scoreEl = document.querySelector('.score');
 const checkBtnEl = document.querySelector('.check');
+const againBtnEl = document.querySelector('.again');
 
 scoreEl.textContent = score;
 console.log(secretNumber);
 checkBtnEl.addEventListener('click', checkNumber);
+againBtnEl.addEventListener('click', resetGame);
 
 function checkNumber() {
   const guess = Number(guessEl.value);
   if (!guess) {
     messageEl.textContent = '🤡 Choice a number.';
   } else if (guess === secretNumber) {
-    updateViewOnGuessSucess();
+    updateDOMOnCorrectGuess();
   } else if (guess > secretNumber) {
     if (score > 1) {
       messageEl.textContent = '📈 Too high!';
@@ -37,6 +39,10 @@ function checkNumber() {
   }
 }
 
+function generateSecretNumber() {
+  return Math.trunc(Math.random() * 20 + 1);
+}
+
 function decreaseScore() {
   score--;
   scoreEl.textContent = score;
@@ -47,9 +53,22 @@ function printGameOverMessage() {
   scoreEl.textContent = 0;
 }
 
-function updateViewOnGuessSucess() {
+function updateDOMOnCorrectGuess() {
   messageEl.textContent = '🎉 Correct number!';
   numberEl.textContent = secretNumber;
   bodyEl.style.backgroundColor = '#60b347';
   numberEl.style.width = '30rem';
+}
+
+function resetGame() {
+  score = 20;
+  secretNumber = generateSecretNumber();
+  console.log(secretNumber);
+  messageEl.textContent = 'Start guessing...';
+  scoreEl.textContent = score;
+  guessEl.value = '';
+  numberEl.textContent = '?';
+
+  bodyEl.style.backgroundColor = '#222';
+  numberEl.style.width = '15rem';
 }
