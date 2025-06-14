a6cfed39d475e9d17445b2752e97b0c2f5c1ce53
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2025-06-13 21:55:28 -0300

    chore: clean up app.component generated by Angular CLI

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,19 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have the 'guess-my-number' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('guess-my-number');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, guess-my-number'
-    );
-  });
 });
